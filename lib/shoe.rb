7d5927a369937e8dd6ae3d78528c7ad8f0ca5f0c
Josh Nho <joshnho@gmail.com>
2019-08-07 03:47:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,13 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS.each { |specific_brand| 
-      if BRANDS.include?(specific_brand)
-        pass
-      else
-        BRANDS << brand
-      end
-    }
+    BRANDS << brand
   end
 
   def cobble
